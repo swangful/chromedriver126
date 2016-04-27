d6582ad9f826847588a30a13cf8f55b97dcd9e0c
Steve Wang <swangful@gmail.com>
2016-04-27 14:01:52 -0700

    1.0.5

--- install.js
+++ install.js
@@ -14,26 +14,12 @@ var url = require('url')
 var util = require('util')
 
 var libPath = path.join(__dirname, 'lib', 'chromedriver')
-var cdnUrl = process.env.npm_config_chromedriver_cdnurl || process.env.CHROMEDRIVER_CDNURL || 'http://chromedriver.googlecode.com/files'
+var cdnUrl = process.env.npm_config_chromedriver_cdnurl || process.env.CHROMEDRIVER_CDNURL || 'http://chromedriver.googlecode.com/files/'
 // adapt http://chromedriver.storage.googleapis.com/
 cdnUrl = cdnUrl.replace(/\/+$/, '')
 var downloadUrl = cdnUrl + 'chromedriver_linux64_26.0.1383.0.zip'
-var platform = process.platform
 
-if (platform === 'linux') {
-  if (process.arch === 'x64') {
-    platform += '64'
-  } else {
-    platform += '32'
-  }
-} else if (platform === 'darwin') {
-  platform = 'mac32'
-} else if (platform !== 'win32') {
-  console.log('Unexpected platform or architecture:', process.platform, process.arch)
-  process.exit(1)
-}
-
-downloadUrl = util.format(downloadUrl, helper.version, platform);
+downloadUrl = util.format(downloadUrl, helper.version);
 
 var fileName = downloadUrl.split('/').pop()
 
